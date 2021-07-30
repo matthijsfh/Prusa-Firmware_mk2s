c72d43e95b51196e3a4a23584f3150ebcbc164b5
3d-gussner <3d.gussner@gmail.com>
2021-07-30 15:00:52 +0200

    Add check_en and check_xx to prevent double messages

--- lang/lang-add.sh
+++ lang/lang-add.sh
@@ -14,23 +14,78 @@ LANGUAGES="cz de es fr it pl"
 # Community languages
 LANGUAGES+=" nl" #Dutch
 
+# Set defaults
+skip_insert="0"
+
+# check single text in english dictionary
+# $1 - text to check
+# $2 - metadata
+check_en()
+{
+if grep "$1" lang_en.txt >/dev/null; then
+    echo "$(tput setaf 3)text already exist in en:"
+    echo "$1$(tput sgr0)"
+    echo
+    skip_insert="1"
+else
+    skip_insert="0"
+fi
+if [ ! -z "$2" ]; then
+    if grep "$2" lang_en.txt >/dev/null; then
+        echo "$(tput setaf 3)Meta already exist in en:"
+        echo "$2$(tput sgr0)"
+        echo
+        skip_insert="1"
+    else
+        skip_insert="0"
+    fi
+fi
+}
 
 # insert single text to english dictionary
 # $1 - text to insert
 # $2 - metadata
 insert_en()
 {
-	#replace '[' and ']' in string with '\[' and '\]'
-	str=$(echo "$1" | sed "s/\[/\\\[/g;s/\]/\\\]/g")
-	# extract english texts, merge new text, grep line number
-	ln=$((cat lang_en.txt; echo "$1") | sed "/^$/d;/^#/d" | sort | grep -n "$str" | sed "s/:.*//;q")
-	# calculate position for insertion
-	ln=$((3*(ln-2)+1))
-	[ "$ln" -lt 1 ] && ln=1
-	# insert new text
-	sed -i "$ln"'i\\' lang_en.txt
-	sed -i "$ln"'i\'"$1"'\' lang_en.txt
-	sed -i "${ln}i\\#$2" lang_en.txt
+    echo "$(tput sgr0)Inserting meta: $(tput setaf 2)$2$(tput sgr0) text: $(tput setaf 2)$1$(tput sgr0) in $(tput setaf 2)en$(tput sgr0)"
+    #replace '[' and ']' in string with '\[' and '\]'
+    str=$(echo "$1" | sed "s/\[/\\\[/g;s/\]/\\\]/g")
+    # extract english texts, merge new text, grep line number
+    ln=$((cat lang_en.txt; echo "$1") | sed "/^$/d;/^#/d" | sort | grep -n "$str" | sed "s/:.*//;q")
+    # calculate position for insertion
+    ln=$((3*(ln-2)+1))
+    [ "$ln" -lt 1 ] && ln=1
+    # insert new text
+    sed -i "$ln"'i\\' lang_en.txt
+    sed -i "$ln"'i\'"$1"'\' lang_en.txt
+    sed -i "${ln}i\\#$2" lang_en.txt
+}
+
+# check single text in translated dictionary
+# $1 - text to check
+# $2 - suffix
+# $3 - metadata
+check_xx()
+{
+if grep "$1" lang_en_$2.txt >/dev/null; then
+    echo "$(tput setaf 3)text already exist in $2:"
+    echo "$1$(tput sgr0)"
+    echo
+    skip_insert="1"
+else
+    skip_insert="0"
+fi
+#if [[ ! -z "$3" && "$skip_insert"="0" ]]; then
+if [ ! -z "$3" ]; then
+    if grep "$3" lang_en_$2.txt >/dev/null; then
+        echo "$(tput setaf 3)Meta already exist in $2:"
+        echo "$3$(tput sgr0)"
+        echo
+        skip_insert="1"
+    else
+        skip_insert="0"
+    fi
+fi
 }
 
 # insert single text to translated dictionary
@@ -39,18 +94,19 @@ insert_en()
 # $3 - metadata
 insert_xx()
 {
-	#replace '[' and ']' in string with '\[' and '\]'
-	str=$(echo "$1" | sed "s/\[/\\\[/g;s/\]/\\\]/g")
-	# extract english texts, merge new text, grep line number
-	ln=$((cat lang_en_$2.txt; echo "$1") | sed "/^$/d;/^#/d" | sed -n 'p;n' | sort | grep -n "$str" | sed "s/:.*//;q")
-	# calculate position for insertion
-	ln=$((4*(ln-2)+1))
-	[ "$ln" -lt 1 ] && ln=1
-	# insert new text
-	sed -i "$ln"'i\\' lang_en_$2.txt
-	sed -i "$ln"'i\"\x00"\' lang_en_$2.txt
-	sed -i "$ln"'i\'"$1"'\' lang_en_$2.txt
-	sed -i "${ln}i\\#$3" lang_en_$2.txt
+    echo "$(tput sgr0)Inserting meta: $(tput setaf 2)$3$(tput sgr0) text: $(tput setaf 2)$1$(tput sgr0) in $(tput setaf 2)$2$(tput sgr0)"
+    #replace '[' and ']' in string with '\[' and '\]'
+    str=$(echo "$1" | sed "s/\[/\\\[/g;s/\]/\\\]/g")
+    # extract english texts, merge new text, grep line number
+    ln=$((cat lang_en_$2.txt; echo "$1") | sed "/^$/d;/^#/d" | sed -n 'p;n' | sort | grep -n "$str" | sed "s/:.*//;q")
+    # calculate position for insertion
+    ln=$((4*(ln-2)+1))
+    [ "$ln" -lt 1 ] && ln=1
+    # insert new text
+    sed -i "$ln"'i\\' lang_en_$2.txt
+    sed -i "$ln"'i\"\x00"\' lang_en_$2.txt
+    sed -i "$ln"'i\'"$1"'\' lang_en_$2.txt
+    sed -i "${ln}i\\#$3" lang_en_$2.txt
 }
 
 # find the metadata for the specified string
@@ -63,26 +119,34 @@ find_metadata()
 
 # check if input file exists
 if ! [ -e lang_add.txt ]; then
-	echo "file lang_add.txt not found"
-	exit 1
+    echo "$(tput setaf 1)file lang_add.txt not found"
+    exit 1
 fi
 
 cat lang_add.txt | sed 's/^/"/;s/$/"/' | while read new_s; do
-	if grep "$new_s" lang_en.txt >/dev/null; then
-		echo "text already exist:"
-		echo "$new_s"
-		echo
-	else
-		meta=$(find_metadata "$new_s")
-		echo "adding text:"
-		echo "$new_s ($meta)"
-		echo
-
-		insert_en "$new_s" "$meta"
+    if grep "$new_s" lang_en.txt >/dev/null; then
+        echo "$(tput setaf 3)text already exist:"
+        echo "$(tput setaf 3)$new_s$(tput sgr0)"
+        echo
+    else
+        meta=$(find_metadata "$new_s")
+        if [ -z "$meta" ]; then
+            echo "$(tput setaf 1)No meta data found for $(tput setaf 3)$new_s$(tput sgr0)"
+        fi
+        echo "$(tput setaf 2)adding text:"
+        echo "$new_s ($meta)$(tput sgr0)"
+        echo
+        check_en "$new_s" "$meta"
+        if [ $skip_insert = "0" ]; then
+            insert_en "$new_s" "$meta"
+        fi
         for lang in $LANGUAGES; do
-            insert_xx "$new_s" "$lang" "$meta"
+            check_xx "$new_s" "$lang" "$meta"
+            if [ $skip_insert = "0" ]; then
+                insert_xx "$new_s" "$lang" "$meta"
+            fi
         done
-	fi
+    fi
 done
 
 read -t 5
